76a08af0affad477af953d83141e264cb074f12c
PipZip <37911683+PipZip@users.noreply.github.com>
2019-11-01 15:55:48 +0600

    temp

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
+    var homeSwiper = new Swiper ('.home-slider .swiper-container', {
+        loop: true,
+        autoHeight: true,
 
+        pagination: {
+            el: '.slider-pagination',
+        },
+
+        navigation: {
+            nextEl: '.slider-button-next',
+            prevEl: '.slider-button-prev',
+        },
+    });
 });
 
 
